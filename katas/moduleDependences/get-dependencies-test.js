734ee1c55ecf8d5b95af00edc5e8e4b6aa52682c
SergeyKovalchuk <sergeyalexkoval@gmail.com>
2016-08-01 16:51:18 +0300

    Update get-dependencies-test.js

--- katas/moduleDependences/get-dependencies-test.js
+++ katas/moduleDependences/get-dependencies-test.js
@@ -18,10 +18,21 @@ describe('module dependencies', function() {
             "version": "1.2.6"
           }
         }
+      },
+      lorem = {
+            "dependencies": {
+              "wordwrap": {
+                "version": "0.0.2",
+              }
+            }
       }
   });
   
   it('should return correct array of dependencies in descending order', function() {
+      expect(getDependencies(loremIpsum)).toEqual([ 'inflection@1.2.6', 'optimist@0.3.7', 'wordwrap@0.0.2']);
+  });
+  
+  it('should return latest version of dependency'), function() {
       expect(getDependencies(loremIpsum)).toEqual([ 'inflection@1.2.6', 'optimist@0.3.7', 'wordwrap@0.0.2']);
   });
 });
