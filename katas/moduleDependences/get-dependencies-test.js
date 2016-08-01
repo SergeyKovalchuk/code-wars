6d61ac446b43f24f54528cacfbb55e2d39700692
SergeyKovalchuk <sergeyalexkoval@gmail.com>
2016-08-01 17:01:02 +0300

    Update get-dependencies-test.js
    add some tests

--- katas/moduleDependences/get-dependencies-test.js
+++ katas/moduleDependences/get-dependencies-test.js
@@ -23,16 +23,32 @@ describe('module dependencies', function() {
             "dependencies": {
               "wordwrap": {
                 "version": "0.0.2",
+                "version": "1.0.2",
+              }
+            }
+      },
+      loremEmpty = {
+            "dependencies": {
+              "wordwrap": {
               }
             }
       }
   });
   
   it('should return correct array of dependencies in descending order', function() {
-      expect(getDependencies(loremIpsum)).toEqual([ 'inflection@1.2.6', 'optimist@0.3.7', 'wordwrap@0.0.2']);
+    expect(getDependencies(loremIpsum)).toEqual([ 'inflection@1.2.6', 'optimist@0.3.7', 'wordwrap@0.0.2']);
   });
   
   it('should return latest version of dependency'), function() {
-      expect(getDependencies(loremIpsum)).toEqual([ 'inflection@1.2.6', 'optimist@0.3.7', 'wordwrap@0.0.2']);
+    expect(getDependencies(lorem)).toEqual([ 'wordwrap@1.0.2']);
+  });
+  
+  it('should have the same character @'), function() {
+    var result = getDependencies(lorem);
+    expect(result[0].slice(-6,-5)).toEqual('@');
+  });
+  
+  it('should return empty array if module doesn\'t have dependencies'), function() {
+    expect(getDependencies(loremEmpty)).toEqual([]);
   });
 });
