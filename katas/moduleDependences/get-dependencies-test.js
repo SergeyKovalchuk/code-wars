a9b8a47de215f79ca672b1742664d1b1bbea1824
SergeyKovalchuk <sergeyalexkoval@gmail.com>
2016-08-01 13:32:53 +0300

    Create get-dependencies-test.js
    add get-dependencies-test

---
+++ katas/moduleDependences/get-dependencies-test.js
@@ -0,0 +1,27 @@
+describe('module dependencies', function() {
+  var testTree;
+  
+  beforeEach(function() {
+      loremIpsum = {
+        "name": "lorem-ipsum",
+        "version": "0.1.1",
+        "dependencies": {
+          "optimist": {
+            "version": "0.3.7",
+            "dependencies": {
+              "wordwrap": {
+                "version": "0.0.2"
+              }
+            }
+          },
+          "inflection": {
+            "version": "1.2.6"
+          }
+        }
+      }
+  });
+  
+  it('should return correct array of dependencies in descending order', function() {
+      expect(getDependencies(loremIpsum)).toEqual([ 'inflection@1.2.6', 'optimist@0.3.7', 'wordwrap@0.0.2']);
+  });
+});
