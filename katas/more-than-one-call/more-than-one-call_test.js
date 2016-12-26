8530908915622dcb5c1b8b341bbf0f3c57de291b
Siarhei.Kavalchuk <sergeyalexkoval@gmail.com>
2016-12-26 12:24:21 +0300

    new task more-than-one-call

--- katas/more-than-one-call/more-than-one-call_test.js
+++ katas/more-than-one-call/more-than-one-call_test.js
@@ -1,8 +1,8 @@
 describe('sum function', function() {
 
     it('should return the sum of the numbers.', function() {
-        Test.expect(sum(2,3), 5);
-        Test.expect(sum(2)(3), 5);
-        Test.expect(sum(3)(3)(1), 7);
+        expect(sum(2,3)).toBe(5);
+        expect(sum(2)(3)).toBe(5);
+        expect(sum(3)(3)(1)).toBe(7);
     });
 });
