76d566a53f327eb281ecc80c55e7beb8f760a4de
SergeyKovalchuk <sergeyalexkoval@gmail.com>
2017-01-23 10:38:29 +0300

    Update array-to-tree_test.js

--- katas/array-to-tree/array-to-tree_test.js
+++ katas/array-to-tree/array-to-tree_test.js
@@ -8,6 +8,6 @@ describe('arrayToTree', function() {
     it('should return correct tree according to the provided array with multiple elements', function() {
         var array = [17, 0, -4, 3, 15];
         var expectedTree = new TreeNode(17, new TreeNode(0, new TreeNode(3), new TreeNode(15)), new TreeNode(-4));
-        expect(array).toEqual(expectedTree);
+        expect(arrayToTree(array)).toEqual(expectedTree);
     });
 });
