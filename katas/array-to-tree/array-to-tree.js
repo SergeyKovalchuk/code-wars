082faa0536f9a8ff48ad41d122d92db37acdb47e
SergeyKovalchuk <sergeyalexkoval@gmail.com>
2017-01-23 10:43:56 +0300

    Update array-to-tree.js

--- katas/array-to-tree/array-to-tree.js
+++ katas/array-to-tree/array-to-tree.js
@@ -1,3 +1,9 @@
+var TreeNode = function(value, left, right) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+};
+
 function arrayToTree(array) {
     return new TreeNode(0); // TODO: implementation
 };
